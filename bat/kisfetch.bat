2bda43f2b63558b8b532b53a6072d320bcca094e
Ross Wan <mrwlwan@gmail.com>
2012-09-06 23:59:52 +0800

    更新kisfetch.py, kisfetch.bat

--- bat/kisfetch.bat
+++ bat/kisfetch.bat
@@ -1,7 +1,3 @@
 @echo off
 
-<<<<<<< HEAD
-python3.exe ../Python/user_scripts/kisfetch.py
-=======
 python3.exe %~dp0/../Python/user_scripts/kisfetch.py
->>>>>>> 4e96ec48729e788568237a77415bebd1ee5a9fa6
