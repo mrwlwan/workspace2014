6ef2d119d2f13743a7257bb984a20a557acba902
Ross Wan <mrwlwan@gmail.com>
2013-03-19 19:17:39 +0800

    Mootstrap: Add Tooltip and Popover.

--- Mootools/Mootstrap/user.js
+++ Mootools/Mootstrap/user.js
@@ -1,9 +1,38 @@
-require(['js/Dropdown', 'js/Collapse', 'js/Alert', 'js/Button', 'js/Tab', 'js/Affix', 'js/ScrollSpy', 'js/Modal', 'domReady!'], function(Dropdown, Collapse, Alert, Button, Tab, Affix, ScrollSpy, Modal){
+require(['js/Dropdown', 'js/Collapse', 'js/Alert', 'js/Button', 'js/Tab', 'js/Affix', 'js/ScrollSpy', 'js/Modal', 'js/Tooltip', 'js/Popover', 'domReady!'], function(Dropdown, Collapse, Alert, Button, Tab, Affix, ScrollSpy, Modal, Tooltip, Popover){
     $$('.bs-docs-sidenav').each(function(el){
         new Affix(el, {
             'offset': {
                 'top': 40,
                 'bottom': 200,
+            }
+        });
+    });
+    $$('.tooltip-demo').each(function(item){
+        new Tooltip(item, {
+            'selector': 'a[data-toggle=tooltip]',
+            'placement': function(el){
+                return el.get('data-placement') || 'top';
+            }
+        });
+    });
+
+    $$('.tooltip-test').each(function(item){
+        new Tooltip(item, {
+            //'container': $$('#myModal .modal-body')[0]
+        });
+    });
+
+    //$$('.popover-test').each(function(item){
+        //new Popover(item, {
+            //'container': item.getParent()
+        //});
+    //});
+
+    $$('a[data-toggle=popover]').each(function(item){
+        new Popover(item, {
+            'container': item.getParent('.bs-docs-example'),
+            'placement': function(el){
+                return el.get('data-placement') || 'right';
             }
         });
     });
