f42ba17c9e5e2985fe8018074baf9def1aaa0fcf
Ross Wan <mrwlwan@gmail.com>
2013-03-22 11:56:24 +0800

    Mootstrap: Update Carousel.js

--- Mootools/Mootstrap/js/Carousel.js
+++ Mootools/Mootstrap/js/Carousel.js
@@ -37,19 +37,14 @@ define(['./utils.js', './more/Elements_from.js'], function(utils){
             }
             this.container.addEvent('click:relay([data-slide])', function(e){
                 e.preventDefault();
-                thisobj.slide(this.get('data-slide'));
+                thisobj[this.get('data-slide')]();
             });
             this.container.addEvent('click:relay([data-slide-to])', function(e){
                 thisobj.to(parseInt(this.get('data-slide-to')));
             });
         },
-        'cycle': function(){
-            if(this.paused && this.options.interval){
-                this.paused = false;
-                if(this.interval) clearInterval(this.interval);
-                this.interval = this.slide.periodical(this.options.interval, this, 'next');
-            }
-            return this;
+        'transitioning': function(){
+            return this.fx && this.fx.isRunning();
         },
         'get_active_item': function(){
             var active;
@@ -62,17 +57,40 @@ define(['./utils.js', './more/Elements_from.js'], function(utils){
             }
             return [null, -1, null];
         },
-        'to': function(pos){
-            var thisobj = this;
+        'cycle': function(){
+            if(this.options.interval){
+                this.paused = false;
+                if(this.interval) clearInterval(this.interval);
+                this.interval = this.next.periodical(this.options.interval, this, true);
+            }
+            return this;
+        },
+        'next': function(cycle){
             var temp = this.get_active_item();
             var active = temp[0];
-            var index = temp[1];
-            var $items = temp[2];
-            if(pos<0 || pos > ($items.length - 1)) return;
-            if(index==pos){
+            var next = active.getNext() || temp[2][0];
+            var next_index = temp[2].indexOf(next);
+            return this.slide('next', active, next, next_index, cycle);
+        },
+        'prev': function(cycle){
+            var temp = this.get_active_item();
+            var active = temp[0];
+            var next = active.getPrevious() || temp[2][temp[2].length-1];
+            var next_index = temp[2].indexOf(next);
+            return this.slide('prev', active, next, next_index, cycle);
+        },
+        'to': function(pos, cycle){
+            var thisobj = this;
+            var temp = this.get_active_item();
+            if(pos<0 || pos > (temp[2].length-1)) return;
+            if(temp[1]==pos){
                 return;
             }
-            return this.slide(pos > index? 'next' : 'prev', $($items[pos]))
+            var active = temp[0];
+            var next = temp[2][pos];
+            var next_index = temp[2].indexOf(next);
+            this.pause();
+            return this.slide(pos > temp[1]? 'next' : 'prev', active, next, next_index, cycle);
         },
         // 暂停循环; 如果没有循环在运行, 则不执行
         'pause': function(e){
@@ -90,32 +108,16 @@ define(['./utils.js', './more/Elements_from.js'], function(utils){
                 next_indicator_item && next_indicator_item.addClass('active');
             }
         },
-        'slide': function(type, next){
+        'slide': function(type, active, next, next_index, cycle){
             // 等待动画完成
-            if(this.fx && this.fx.isRunning()){
-                return utils.one(this, 'slid', this.slide.pass([type, next], this));
+            if(!cycle) this.pause();
+            if(this.transitioning()){
+                return utils.one(this, 'slid', this.slide.pass([type, active, next, next_index, cycle], this));
             }
-            if(next) this.pause();
             var direction = type == 'next' ? 'left' : 'right';
-            var fallback  = type == 'next' ? 'first' : 'last';
+            //var fallback  = type == 'next' ? 'first' : 'last';
             var thisobj = this;
-            var temp = this.get_active_item();
-            var active = temp[0];
-            var index = temp[1];
-            var $items = temp[2];
-            if(!next){
-                switch(type){
-                    case 'next':
-                        next = active.getNext() || $items[0];
-                        break;
-                    case 'prev':
-                        next = active.getPrevious() || $items[$items.length-1];
-                        break;
-                }
-            }
-            if(next.hasClass('active')) return;
             this.fireEvent('slide', [active, next, direction]);
-            var next_index = $items.indexOf(next);
             if(this.options.transition){
                 this.transition(active, next, next_index, function(){
                     thisobj.fireEvent('slid', [active, next, direction]);
@@ -127,18 +129,20 @@ define(['./utils.js', './more/Elements_from.js'], function(utils){
                 thisobj.fireEvent('slid', [active, next, direction]);
             }
             // 重新进入循环
-            if(this.paused) this.cycle();
+            if(cycle) this.cycle();
         },
         'transition': function(active, next, next_index, callback){
             var thisobj = this;
+            active.removeClass('active');
+            active.setStyle('display', 'block');
+            next.addClass('active');
+            next.setStyle('display', 'none');
             this.fx = new Fx.Tween(active, {
                 'property': 'opacity',
                 'duration': this.options.duration*0.4,
                 'onComplete': function(){
                     next.setStyle('display', 'block');
                     active.setStyle('display', 'none');
-                    active.removeClass('active');
-                    next.addClass('active');
                     thisobj.slide_indicator(next_index);
 
                     thisobj.fx = new Fx.Tween(next, {
@@ -174,7 +178,7 @@ define(['./utils.js', './more/Elements_from.js'], function(utils){
         if(index){
             carousel.to(parseInt(index));
         }else{
-            carousel.slide(this.get('data-slide'));
+            carousel[this.get('data-slide')]();
         }
     });    
 
